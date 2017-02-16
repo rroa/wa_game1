ce4990cb2f8bbbab2b04731ebf74929ad11dfb93
Raul Roa <raul.roa@gmail.com>
2017-02-16 11:09:43 -0400

    Added wrap around

--- BoilerPlate/Ship.cpp
+++ BoilerPlate/Ship.cpp
@@ -12,7 +12,24 @@ namespace Asteroids
 {
 	namespace Entities
 	{
+		inline float wrap(float axis, float min, float max)
+		{
+			if (axis < min)
+			{
+				return max - (min - axis);
+			}
+
+			if (axis > max)
+			{
+				return min + (axis - max);
+			}
+
+			return axis;
+		}
+
 		const float ANGLE_OFFSET = 90.0f;
+		const int WIDTH = 1136;
+		const int HEIGHT = 640;
 
 		Ship::Ship(const std::vector<Engine::Math::Vector2> points)
 			: m_points(points)
@@ -30,6 +47,21 @@ namespace Asteroids
 			
 			Engine::Math::Vector2 newPos =
 				m_position + velocity;
+
+			float halfWidth = (WIDTH / 2.0f);
+			float halfHeight = (HEIGHT / 2.0f);
+
+			float worldMinX = -halfWidth;
+			float worldMaxX = halfWidth;
+
+			float worldMinY = -halfHeight;
+			float worldMaxY = halfHeight;
+
+			// Wrap!
+			float x = wrap(newPos.GetX(), worldMinX, worldMaxX);
+			float y = wrap(newPos.GetY(), worldMinY, worldMaxY);
+
+			newPos = Engine::Math::Vector2(x, y);
 
 			Entity::Translate(newPos);
 		}
